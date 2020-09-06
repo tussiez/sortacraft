edb267a814227f21c953799a77911d5d4d8b7d87
tussiez <49251771+tussiez@users.noreply.github.com>
2020-09-06 12:58:02 -0400

    kill shadows

--- geometrydataworker.js
+++ geometrydataworker.js
@@ -323,9 +323,6 @@ onmessage = function(e){
     const {positions,normals,uvs,indices,faceIndexGroup,faceIndex} = voxelWorld.generateGeometryDataForCell(e.data[1]/64,e.data[2]/64,e.data[3]/64);//heavy loading
     //this will cover chunk bounds
     //calculate faceindxgroup
-    for(var face = 0; face<faceIndex;face++){
-      
-    }
     postMessage(['geometrydata',positions,normals,uvs,indices,e.data[4],e.data[5]]);//e.data[2] if pos
   }
 }
