2f383b778a890d2934a75c6e2b0131b70ba88714
tussiez <49251771+tussiez@users.noreply.github.com>
2020-09-22 18:12:27 -0400

    texture width set (not power of 2??)

--- geometrydataworker.js
+++ geometrydataworker.js
@@ -284,7 +284,7 @@ VoxelWorld.faces = [
 const voxelWorld = new VoxelWorld({
   cellSize:64,
   tileSize:16,
-  tileTextureWidth:464,
+  tileTextureWidth:671,
   tileTextureHeight:48,
   calculateFaces:true
 });//prep voxelwordl
