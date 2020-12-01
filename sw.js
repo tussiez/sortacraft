64820a611a82ac370c9d6f75db6577385ba16908
Titus Ramsarran <109titus@gmail.com>
2020-12-01 20:32:44 +0000

    icon

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
 /*This is the service worker file. It will be registered as a service worker. Service workers and Web workers both work on seperate threads than the main thread, which is used to handle all other JavaScript files otherwise.*/
 /*In a service worker, it uses the global scope of "self". This is like the window object in main frames. Note that you can't access local storage or session storage, or the window object. You could rewrite self as "this" as well.*/
 
+
 /*control current page when it's activated (A.K.A, whenever it is run). Service workers run when there is a request of any kind, and stop when they aren't needed. So, when a user requests a webpage, it will activate. That is why it's like a proxy for the website.*/
 
 self.addEventListener("activate", function (event) {
-
+  /*Claim the client (page)*/
   return self.clients.claim();
 
 });
@@ -18,7 +19,7 @@ self.addEventListener("install", function (event) {
   caches.open("cache").then(function (cache) {
 
     /*Adds the necessary files to cache*/
-    cache.addAll(["index.html", "chunkworker.js", "daynight.js", "dirt.png", "gameworker.js", "geometrydataworker.js", "home.js", "inv.js", "newgame.html", "newgame.js", "script.js", "style.css", "textures.png", "tween.js"]);
+    cache.addAll(["index.html", "chunkworker.js", "daynight.js", "dirt.png", "gameworker.js", "geometrydataworker.js", "home.js", "inv.js", "newgame.html", "newgame.js", "script.js", "style.css", "textures.png", "tween.js","ico.ico"]);
 
   });
 
@@ -65,6 +66,33 @@ self.addEventListener("fetch", function (event) {
       });
 
     }
-    
+    else {
+      /*Otherwise, try fetching the script first, then return it. If the user is offline, then try to serve it from cache. If not, tell them that something went wrong (since the cache doesn't exist or is bad and they aren't online) */
+      return fetch(event.request).then(function (res) {
+
+        return res;
+
+      }).catch(function () {
+
+        return caches.match(event.request).then(function (cacheRes) 
+        {
+
+          if (cacheRes) {
+
+            return cacheRes;
+
+          }
+
+          else {
+            /*Return the response telling them that something went wrong. */
+            return new Response("<!DOCTYPE html> <html lang='en'> <head> <meta charset='UTF-8'> <title>Error fetching cache</title>  <meta charset='UTF-8'> <meta name='viewport' content='width=device-width, initial-scale=1'> <meta name='description' content='Error'></head> <body><h1>Error fetching cache. Try going online. If that doesn't work, try clearing the site's data.</h1></body> </html>", { status: 520, statusText: "Error fetching cache", headers: new Headers({ "Content-Type": "text/html" }) });
+            //Interesting
+          }
+
+        });
+
+      });
+
+    }
   })());
 });
---
+++ swHandler.js
@@ -0,0 +1,1 @@
+/*This handles the service worker (registering it) */
